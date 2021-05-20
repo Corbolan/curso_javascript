98f96e655e5bcd689baf482984b5b2e3d79c479f
Germano Flores Corbolan <gecorbolan@gmail.com>
2021-05-19 23:46:48 -0300

    Fundamentos Javascript

---
+++ Function/parameters.js
@@ -0,0 +1,38 @@
+function logParams (a, b, c){
+    console.log(a, b, c)
+}
+
+logParams(1)
+logParams(1, 2)
+logParams(1, 2, 3)
+logParams(1, 2, 3, 4, 5)
+
+function defaultParams(a = 1, b = 2, c = 3){
+    console.log(a, b, c)
+}
+
+defaultParams(7, 8, 9)
+defaultParams(7, 8)
+defaultParams(7)
+defaultParams()
+
+console.log(1)
+console.log(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+// spreaad/rest
+function logNums(...nums) {
+    for(let n of nums) {
+        console.log(n)
+    }
+}
+
+logNums([1, 2, 3, 4, 5, 6])
+
+function sumAll(...nums){
+    let total = 0
+    for(let n of nums){
+        
+    }
+}
+
+
